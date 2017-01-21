d865cc2684da89ff18883df2261a972416f9f8a0
rtole999 <rtole99@gmail.com>
2017-01-21 06:29:33 -0600

    adding buttons

--- MirclePill/ViewController.swift
+++ MirclePill/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var statePicker: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
